cbbb64d69a8b6c612c75e73f6f016a7f18a55c2c
LeoSheesh <113630581+LeoSheesh@users.noreply.github.com>
2022-10-01 18:36:25 -0400

    main avec args parser commandline

--- Program.cs
+++ Program.cs
@@ -1,40 +1,56 @@
 ﻿static class Constants{
     public const int DIMENSION = 5;
     public const int NBOFROOM = DIMENSION*DIMENSION;
+     // = ops.DImension au carré
 
-    public static bool isASquare(){
-
-        for (int i = 1; i <= (int)Math.Sqrt((double)NBOFROOM); i++){
-            if(i*i == NBOFROOM){
-                return true;
-            }
-        }
-        return false;
-    }
+    
 }
 
-// Programmes
-class Program{
-    static void Main()
-    {
-        // To do 
 
-        // Test
-        Environment myEnvironment = new Environment();
-        Robot myRobot = new Robot();
-        /*
-        myEnvironment.life();
-        Console.WriteLine(myRobot.getElectricity());
-        myRobot.aspire(myEnvironment);
-        Console.WriteLine(myRobot.getElectricity());
-        */
-        /*
-        for (int i = 0; i < 100; i++)
+
+namespace Agent_aspirateur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
         {
-            Console.WriteLine("\n\n\n");
-            myEnvironment.life();
-            myEnvironment.print();
+
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(ops =>
+                   {
+
+                       if (ops.Dimension == null || ops.Dimension < 1)
+                       {
+                           Console.WriteLine("You need to indicate the number of rooms in width and lenght that is and integer higher than 0");
+                       }
+                       else
+                       {
+                           perform_actions(ops);
+                       }
+
+
+                   });
         }
-        */
+
+    // To do 
+
+    // Test
+    Environment myEnvironment = new Environment();
+    Robot myRobot = new Robot();
+    /*
+    myEnvironment.life();
+    Console.WriteLine(myRobot.getElectricity());
+    myRobot.aspire(myEnvironment);
+    Console.WriteLine(myRobot.getElectricity());
+    */
+    /*
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine("\n\n\n");
+        myEnvironment.life();
+        myEnvironment.print();
     }
+    */
+    
+
 }
